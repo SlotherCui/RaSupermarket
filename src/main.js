88c914ad209b71da1f4adbe7252ba4b625757aa2
SlotherCui <35825578+SlotherCui@users.noreply.github.com>
2019-06-20 13:37:16 +0800

    修改elementUI的相关配置

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import 'element-ui/lib/theme-chalk/index.css'
 import ElementUI from 'element-ui'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,5 +1,13 @@
 <template>
-  <div>主界面2</div>
+
+  <el-container direction="horizontal">
+    <el-aside >Aside</el-aside>
+    <el-container>
+      <el-header>Header</el-header>
+      <el-main>Main</el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -9,5 +17,24 @@ export default {
 </script>
 
 <style scoped>
+/*.el-header, .el-footer {*/
+  /*background-color: #B3C0D1;*/
+  /*color: #333;*/
+  /*text-align: center;*/
+  /*line-height: 60px;*/
+/*}*/
 
+/*.el-aside {*/
+  /*background-color: #D3DCE6;*/
+  /*color: #333;*/
+  /*text-align: center;*/
+  /*line-height: 200px;*/
+/*}*/
+
+/*.el-main {*/
+  /*background-color: #E9EEF3;*/
+  /*color: #333;*/
+  /*text-align: center;*/
+  /*line-height: 160px;*/
+/*}*/
 </style>
