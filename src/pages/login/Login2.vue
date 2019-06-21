70757f8c3d3dd5272d87d468e82d879276242849
Hun1k <1007761500@qq.com>
2019-06-21 10:47:51 +0800

    621张翰林登录完善

--- src/pages/login/Login2.vue
+++ src/pages/login/Login2.vue
@@ -1,21 +1,25 @@
 <template>
-  <div class="backgroundimage">
+  <div >
+    <div class="backgroundimage" v-show="!show">
+      <img src="/static/Login1.png" style="width: 100%;height: 100%"/>
+    </div>
     <div class="touming" v-show="show">
+      <img src="/static/Login1.png" style="width: 100%;height: 100%"/>
     </div>
     <div class="Loginbox" @mouseenter="show=!show" @mouseleave="show=!show">
-      <el-form ref="form" :model="form" >
+      <el-form ref="form" :model="form" :rules="rules"  class="demo-ruleForm login-container">
         <h2>用户登录</h2>
-        <el-form-item>
-          <el-input class="input" placeholder="用户名" suffix-icon="el-icon-user" v-model="form.UserName" clearable></el-input>
+        <el-form-item prop="UserName">
+          <el-input class="input" auto-complete="off" placeholder="用户名" prefix-icon="el-icon-user" v-model="form.UserName" clearable></el-input>
         </el-form-item>
-        <el-form-item>
-          <el-input class="input" placeholder="请输入密码" suffix-icon="el-icon-key" v-model="form.Password" show-password></el-input>
+        <el-form-item prop="Password">
+          <el-input class="input" auto-complete="off" placeholder="密码" prefix-icon="el-icon-key" v-model="form.Password" show-password></el-input>
         </el-form-item>
         <el-form-item class="remeber">
           <el-checkbox label="记住密码" v-model="form.Remember"></el-checkbox>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" plain @click="todologin" class="LonginButton">登录</el-button>
+          <el-button type="primary" plain @click="Logincheck" class="LonginButton">登录</el-button>
         </el-form-item>
       </el-form>
     </div>
@@ -34,11 +38,32 @@ export default {
         Password: '',
         Remember: false
       },
+      rules: {
+        UserName: [{
+          required: true,
+          message: '请输入账号',
+          trigger: 'blur'
+        } ],
+        Password: [{
+          required: true,
+          message: '请输入密码',
+          trigger: 'blur'
+        } ]
+      },
       show: false
     }
   },
   methods: {
-    todologin () {
+    Logincheck () {
+      if ((this.form.Password.length !== 0) && (this.form.UserName.length !== 0)) {
+        console.log(this.form.UserName + this.form.Password + this.form.Remember)
+      } else {
+        alert('请输入有效账号和密码！')
+        this.form.UserName = ''
+        this.form.Password = ''
+      }
+    },
+    Todologin () {
       console.log(this.form.UserName + this.form.Password + this.form.Remember)
       // 测试接口
       var Params = {page: 1}
@@ -53,26 +78,39 @@ export default {
 <style scoped>
   .backgroundimage{
     position: absolute;
-    margin-top: -60px;
-    margin-left: -10px;
+    margin-top: -9px;
+    margin-left: -8px;
     width: 100%;
     height: 100%;
-    background: url(/static/timg.jpg);
-
+    /*-ms-transform: translate(-50px, -100px);*/
+    /*-webkit-transform: translate(-50px, -100px);*/
+    /*transform: translate(-50px, -100px);*/
+    /*-webkit-animation:run 6s linear 0s infinite;*/
   }
   .touming{
     z-index: 0;
     position: absolute;
+    margin-top: -9px;
+    margin-left: -8px;
     height: 100%;
     width: 100%;
-    background: url(/static/timg.jpg);
-    -webkit-filter: blur(10px)
+
+    -webkit-filter: blur(10px);
   }
+  .touming img{
+    cursor: pointer;
+    transition: all 0.6s;
+  }
+  .touming img:hover{
+    transform: scale(1.3,1.3);
+  }
+
   .input{
     height: 1px;
   }
   .Loginbox{
     z-index: 10;
+    border-radius: 5px;
     position: absolute;
     top:0;
     bottom: 0;
