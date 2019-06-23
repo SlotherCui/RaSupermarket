2b5b41034b7d6f039274a7b5be71a6db9c6aa82d
Hun1k <1007761500@qq.com>
2019-06-23 10:56:14 +0800

    语言

--- src/common/lang/en.js
+++ src/common/lang/en.js
@@ -9,7 +9,13 @@ const en = {
     'shop_change_price': 'Goods price change',
     'shop_change_relation': 'Price Change Relation',
     'common_goods': 'Common Goods browsing',
-    'My_goods': 'My Goods Manage'
+    'My_goods': 'My Goods Manage',
+    'bar_code': 'Bar Code',
+    'query': 'Query',
+    'add': 'Add',
+    'goods': 'Goods',
+    'price': 'Price',
+    'operation': 'Operation'
   },
   ...enLocale
 }
