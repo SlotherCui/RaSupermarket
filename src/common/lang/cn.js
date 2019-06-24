b3baf869a99daa5148c0bfde8db46b2b3dc0084f
SlotherCui <35825578+SlotherCui@users.noreply.github.com>
2019-06-24 13:40:08 +0800

    明确字段名称

--- src/common/lang/cn.js
+++ src/common/lang/cn.js
@@ -9,7 +9,7 @@ const cn = {
     'chart': '图表统计',
     'shop_change_price': '商品改价功能',
     'shop_change_relation': '改价关系维护',
-    'common_goods': '公共商品浏览',
+    'common_goods': '供应商品浏览',
     'My_goods': '商家商品维护',
     // 商品表表头
     'goods_barcode': '条码号',
@@ -18,11 +18,22 @@ const cn = {
     'goods_price': '价格',
     'goods_describe': '商品描述',
     'goods_supplier_id': '供应商编号',
+    // 更新
+    'commodity_barcode': '条码号',
+    'commodity_name': '商品名称',
+    'commodity_specification': '规格型号',
+    'commodity_price': '价格',
+    'commodity_description': '商品描述',
     // 商品详情,依然需要带上以上商品表头信息
     'goods_brand': '商品商标',
     'goods_producer': '商品厂家',
     'goods_img': '商品图片',
     'goods_update_time': '修改时间',
+    // 更新
+    'commodity_brand': '商品商标',
+    'commodity_producer': '商品厂家',
+    'commodity_piclink': '商品图片',
+    'create_time': '修改时间',
     // 销售记录
     'order_id': '销售编号',
     'order_price': '销售总价',
@@ -35,6 +46,7 @@ const cn = {
     'order_goods_num': '产品个数',
     'order_goods_price': '产品价格',
     // 商家信息
+    'user_id': '超市编号',
     'supermarket_img': '超市头像',
     'supermarket_name': '超市名称',
     'supermarket_manager_name': '超市负责人名称',
@@ -48,6 +60,7 @@ const cn = {
     'query': '查询',
     'add': '新增',
     'operation': '操作',
+    'open': '开启',
     'edit': '编辑',
     'delete': '删除',
     'batchDelete': '批量删除',
--- src/pages/home/nav1/Relationship.vue
+++ src/pages/home/nav1/Relationship.vue
@@ -7,10 +7,10 @@
           <el-input v-model="filters.name" placeholder="请输入商家编号"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" v-on:click="getUsers">查询</el-button>
+          <el-button type="primary" v-on:click="getUsers">{{$t('message.query')}}</el-button>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="handleAdd">新增</el-button>
+          <el-button type="primary" @click="handleAdd">{{$t('message.add')}}</el-button>
         </el-form-item>
       </el-form>
     </el-col>
@@ -21,60 +21,32 @@
       </el-table-column>
       <!--<el-table-column type="index" width="60">-->
       <!--</el-table-column>-->
-      <el-table-column prop="shop_id" label="商家编号" width="150" sortable>
+      <el-table-column prop="user_id" :label="$t('message.user_id')" width="150" sortable>
       </el-table-column>
-      <el-table-column prop="shop_name" label="商家名称" width="150" sortable>
+      <el-table-column prop="supermarket_name" :label="$t('message.supermarket_name')"  width="150" sortable>
       </el-table-column>
-      <el-table-column prop="shop_phone" label="商家电话"  width="150" sortable>
+      <el-table-column prop="supermarket_tel" :label="$t('message.supermarket_tel')"  width="150" sortable>
       </el-table-column>
-      <el-table-column prop="shop_addr" label="商家地址" min-width="150" sortable>
+      <el-table-column prop="supermarket_address" :label="$t('message.supermarket_address')" min-width="150" sortable>
       </el-table-column>
-      <el-table-column prop="shop_describe" label="商家描述" min-width="180" sortable>
+      <el-table-column prop="supermarket_decription" :label="$t('message.supermarket_decription')" min-width="180" sortable>
       </el-table-column>
       <!--<el-table-column prop="addr" label="地址" min-width="180" sortable>-->
       <!--</el-table-column>-->
-      <el-table-column label="操作" width="150">
+      <el-table-column :label="$t('message.operation')" width="150">
         <template scope="scope">
-          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">开启</el-button>
-          <el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">删除</el-button>
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">{{$t('message.open')}}</el-button>
+          <el-button type="danger" size="small" @click="handleDel(scope.$index, scope.row)">{{$t('message.delete')}}</el-button>
         </template>
       </el-table-column>
     </el-table>
 
     <!--工具条-->
     <el-col :span="24" class="toolbar">
-      <el-button type="danger" @click="batchRemove" :disabled="this.sels.length===0">批量删除</el-button>
+      <el-button type="danger" @click="batchRemove" :disabled="this.sels.length===0">{{$t('message.batchDelete')}}</el-button>
       <el-pagination layout="prev, pager, next" @current-change="handleCurrentChange" :page-size="20" :total="total" style="float:right;">
       </el-pagination>
     </el-col>
-
-    <!--编辑界面-->
-    <el-dialog title="编辑" v-model="editFormVisible" :close-on-click-modal="false">
-      <el-form :model="editForm" label-width="80px" :rules="editFormRules" ref="editForm">
-        <el-form-item label="姓名" prop="name">
-          <el-input v-model="editForm.name" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="性别">
-          <el-radio-group v-model="editForm.sex">
-            <el-radio class="radio" :label="1">男</el-radio>
-            <el-radio class="radio" :label="0">女</el-radio>
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item label="年龄">
-          <el-input-number v-model="editForm.age" :min="0" :max="200"></el-input-number>
-        </el-form-item>
-        <el-form-item label="生日">
-          <el-date-picker type="date" placeholder="选择日期" v-model="editForm.birth"></el-date-picker>
-        </el-form-item>
-        <el-form-item label="地址">
-          <el-input type="textarea" v-model="editForm.addr"></el-input>
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click.native="editFormVisible = false">取消</el-button>
-        <el-button type="primary" @click.native="editSubmit" :loading="editLoading">提交</el-button>
-      </div>
-    </el-dialog>
 
     <!--新增界面-->
     <el-dialog title="新增" v-model="addFormVisible" :close-on-click-modal="false">
@@ -117,34 +89,17 @@ export default {
       },
       users: [],
       sells: [{
-        shop_id: 124547,
-        shop_name: '附近超市',
-        shop_addr: '山东省济南市历下区舜华路1500号',
-        shop_phone: 18340018831,
-        shop_describe: '山东大学软件学院学生生活超市'
+        user_id: 124547,
+        supermarket_name: '附近超市',
+        supermarket_address: '山东省济南市历下区舜华路1500号',
+        supermarket_tel: 18340018831,
+        supermarket_decription: '山东大学软件学院学生生活超市'
 
       }],
       total: 0,
       page: 1,
       listLoading: false,
       sels: [], // 列表选中列
-
-      editFormVisible: false, // 编辑界面是否显示
-      editLoading: false,
-      editFormRules: {
-        name: [
-          { required: true, message: '请输入姓名', trigger: 'blur' }
-        ]
-      },
-      // 编辑界面数据
-      editForm: {
-        id: 0,
-        name: '',
-        sex: -1,
-        age: 0,
-        birth: '',
-        addr: ''
-      },
 
       addFormVisible: false, // 新增界面是否显示
       addLoading: false,
