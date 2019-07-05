9c8d0af56edf948708fe53f1681c7ae7469920c2
Hun1k <1007761500@qq.com>
2019-07-05 09:23:48 +0800

    mygoods - debug
    翻页，查询

--- src/pages/home/nav2/sells.vue
+++ src/pages/home/nav2/sells.vue
@@ -316,7 +316,6 @@ export default {
     },
     // 请求销售记录升级版
     getOrderListNew (page) {
-      console.log('输入here')
       this.listLoading = true
       var min = this.filters.price[0]
       if (min === '') {
